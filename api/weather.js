8e01eb53f012e1cc52a08f8fa9d63f732ff29952
RichardaBall <99812305+RichardaBall@users.noreply.github.com>
2025-07-05 10:53:53 +0100

    Update weather.js

--- api/weather.js
+++ api/weather.js
@@ -1,8 +1,9 @@
 export default async function handler(req, res) {
-  const { lat, lon, key } = req.query;
+  const { lat, lon } = req.query;
+  const key = 'mpwturrttqmcb4ax95xo94feri15h24bzcj6736m';
 
-  if (!lat || !lon || !key) {
-    res.status(400).json({ error: 'Missing parameters' });
+  if (!lat || !lon) {
+    res.status(400).json({ error: 'Missing lat or lon' });
     return;
   }
 
@@ -11,13 +12,13 @@ export default async function handler(req, res) {
   try {
     const response = await fetch(url);
     if (!response.ok) {
-      res.status(response.status).json({ error: 'Error fetching weather API' });
+      res.status(response.status).json({ error: 'Error fetching Meteosource API' });
       return;
     }
     const data = await response.json();
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 }
